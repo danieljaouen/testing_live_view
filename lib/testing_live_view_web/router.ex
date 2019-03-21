67ac5f8b22499141275fc4118e10da3a5b1be1fe
Daniel Jaouen <dcj24@cornell.edu>
2019-03-20 23:59:43 -0400

    Add put_layout plug

--- lib/testing_live_view_web/router.ex
+++ lib/testing_live_view_web/router.ex
@@ -8,6 +8,7 @@ defmodule TestingLiveViewWeb.Router do
     plug Phoenix.LiveView.Flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :put_layout, {TestingLiveViewWeb.LayoutView, :app}
   end
 
   pipeline :api do
